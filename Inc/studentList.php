9f3fcb404c62a1c390ac24e39fd78debb9762f46
nasim114 <nasimuddin1140@gmail.com>
2022-10-29 01:41:23 +0600

    add student page design

---
+++ Inc/studentList.php
@@ -0,0 +1,22 @@
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="panel-title">Student List</h3>
+            <a href="add-student.php" class="btn btn-primary pull-right">Add Student</a>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
---
+++ add-student.php
@@ -0,0 +1,25 @@
+<?php include 'inc/header.php'; ?>
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            <div class="md-6" style="float: left;"><h3 class="panel-title pull-right">Add Student</h3></div>
+            <div class="md-6 pull-right"style="float: right;"><a href="index.php" class="btn btn-primary">Student List</a></div>
+        </div>
+        <div class="card-body">
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary mt-2">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<?php include 'inc/footer.php'; ?>
